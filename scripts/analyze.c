46a296f7ea1300a97e74b3c8e8316e58eb205181
Raino <rainokanne74@gmail.com>
2025-04-19 15:09:06 +0300

    worked on analyze.c

--- scripts/analyze.c
+++ scripts/analyze.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+#define BUFFER_SIZE 1024
+
 int main(void)
 {
-    FILE* file = fopen("uploads/raw/without_me.mp3", "r");
+
+    unsigned char buffer[BUFFER_SIZE];   // Setting buffer syze to 1 kilobyte.
+
+    FILE* file = fopen("uploads/raw/Till i Collapse.mp3", "rb");  // this needs to dynamically choose the file later
+    if (file == NULL)
+    {
+        return 1;
+    }
+    while (fread(buffer, 1, BUFFER_SIZE, file) > 0) 
+    {
+        // first i need to verify the file format
+        for (int i = 0; i < BUFFER_SIZE - 1; i++) 
+        {
+            if (buffer[i] == 0xFF && (buffer[i+1] & 0xE0) == 0xE0) 
+            {
+                printf("Found a frame at buffer index %d\n", i);
+
+
+            }
+        }
+        
+    }
 
     return 0;
     
