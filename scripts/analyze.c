238d05e1e623fbcf200c8ee8442604d50bb7e645
Raino <rainokanne74@gmail.com>
2025-04-21 10:39:03 +0300

    added mpeg version lookup table

--- scripts/analyze.c
+++ scripts/analyze.c
@@ -6,6 +6,14 @@ int main(void)
 {
     unsigned char buffer[BUFFER_SIZE];   // Setting buffer syze to 1 kilobyte.
 
+    const char* mpeg_versions[] =   // Lookup table for mpeg versions
+    {
+        "MPEG Version 2.5",  // 00
+        "reserved",          // 01
+        "MPEG Version 2",    // 10
+        "MPEG Version 1"     // 11
+    };
+
     FILE* file = fopen("../uploads/raw/song.mp3", "rb");  // this needs to dynamically choose the file later
     if (file == NULL)
     {
@@ -20,6 +28,7 @@ int main(void)
         {
             if (buffer[i] == 0xFF && (buffer[i+1] & 0xE0) == 0xE0) 
             {
+                // Here i have found frame header
                 unsigned char b1 = buffer[i];
                 unsigned char b2 = buffer[i + 1];
                 unsigned char b3 = buffer[i + 2];
@@ -29,11 +38,14 @@ int main(void)
                 int layer_description = (b2 & 0x06) >> 1;
                 int bitrate_index = (b3 & 0xF0) >> 4;
                 int sampling_rate_index = (b3 & 0x0C) >> 2;
+                // Looking up these actual values
+                const char* version = mpeg_versions[mpeg_version_id];
+
 
                 counter++;
                 //printf("Found frame %i at buffer index %d\n", counter, i);
-                printf("MPEG version: %d, Layer: %d, Bitrate index: %d, Sampling rate index: %d\n",
-                    mpeg_version_id, layer_description, bitrate_index, sampling_rate_index);
+                printf("MPEG: %s, Layer: %d, Bitrate index: %d, Sampling rate index: %d\n",
+                    version, layer_description, bitrate_index, sampling_rate_index);
 
 
             }
