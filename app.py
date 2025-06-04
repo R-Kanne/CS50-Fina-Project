070dbae8c2a661a71d35818223343ab14884b372
Raino <rainokanne74@gmail.com>
2025-06-04 15:31:42 +0300

    Setting up structure for file saving in the server

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, jsonify, redirect, render_template, request, session
 from flask_session import Session
 import os
 import subprocess
 from werkzeug.utils import secure_filename
+import json
 
 # Configure application
 app = Flask(__name__)
@@ -16,18 +17,27 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB max file size
 ALLOWED_EXTENSIONS = {'mp3'}
 
 def allowed_file(filename):
+    """ Return True if filename has an allowed extension."""
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    """The main page of the website"""
+    """The main page of the website, handles file uploads and analysis."""
     if request.method == 'POST':
-        file = request.files['file']
-        # TODO i should validate that it is an mp3 file.
-        # Really think trough the security aspect of receiving an unknown file to your server.
-        file.save('uploads/raw/' + file.filename)
-        return render_template("index.html")
+         if 'file' not in request.files: # Assuming the input field name is 'mp3_file'
+            return jsonify({"error": "No file part in the request"}), 400
+         
+         file = request.files['file']  # Getting the submitted file
+
+         if file and allowed_file(file.filename):
+            # Securely save the filename to prevent directory traversal attacks
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+         # TODO i should validate that it is an mp3 file.
+         # Really think trough the security aspect of receiving an unknown file to your server.
+         return render_template("index.html")
     return render_template("index.html")
     
 
